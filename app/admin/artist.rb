9dd35d448898f0f2989ef0cd5225abd193bd5be5
Seth Lachman <lachmanseth17@gmail.com>
2016-06-09 20:12:39 -0400

    Done.

---
+++ app/admin/artist.rb
@@ -0,0 +1,7 @@
+ActiveAdmin.register Artist do
+  permit_params :name
+  actions :all, except: [:destroy]
+
+
+
+end
---
+++ app/admin/song.rb
@@ -0,0 +1,6 @@
+ActiveAdmin.register Song do
+  permit_params :title, :artist_id
+  
+
+
+end
