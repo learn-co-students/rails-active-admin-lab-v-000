5b1c4fa1bead06793ad153afbecf2c8a1e52a671
cassaram09 <cassaram09@gmail.com>
2016-07-26 17:41:06 -0400

    Done.

--- app/admin/artist.rb
+++ app/admin/artist.rb
@@ -1,13 +1,5 @@
 ActiveAdmin.register Artist do
   permit_params :name
-  actions :show
-
-  form do |f|
-    inputs 'Author' do
-      f.input :name
-    end
-    f.semantic_errors
-    f.actions
-  end
+  actions :all, except: [:destroy]
 
 end
