bb25c5349089a6922c1fedf95590838c39f6ab31
ndaniele <n.daniele@gmail.com>
2016-10-06 17:51:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  #resources :artists, :songs
+  resources :artists, only: [:index, :show] 
+  resources :songs, only: [:index, :show]
 
   root to: 'artists#index'
+
+  #get '/artists/new/:id', to: 'artists#show', id: 'new'
+
+  #get '/artists/new' => {:id=>"new", :controller=>"artists", :action=>"show"}
 end
