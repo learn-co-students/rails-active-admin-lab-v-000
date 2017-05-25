a8dd5e6eeb85d285116f37428a2c817c9a104005
Ray Mulligan <rmullig2@optonline.net>
2017-05-25 01:22:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  resources :artists, except: [:new, :create, :update]
+  resources :songs, except: [:new, :create, :update]
 
   root to: 'artists#index'
+  
+  
+  get '/artists/new', to: 'artists#show'
+  get '/songs/new', to: 'songs#show'
 end
