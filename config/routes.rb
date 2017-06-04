0ccb3fbd33dd634ab8dd36365540cd1824c61785
Kelsey Huse <kelseyhuse30@gmail.com>
2017-06-04 16:57:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  resources :artists, :songs, except: [:update, :edit, :new, :create, :destroy]
 
   root to: 'artists#index'
 end
