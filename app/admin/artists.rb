a481fc0dea7ed23227bfadec5a570613dd86ae78
Allen Anderson <dande313@byu.net>
2017-10-24 19:32:47 +0000

    Done.

---
+++ app/admin/artists.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Artist do
+    permit_params :name
+    actions :all, except: [:destroy]
+end
---
+++ app/admin/songs.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Song do
+  permit_params :title
+  actions :all, except: [:destroy]
+end
