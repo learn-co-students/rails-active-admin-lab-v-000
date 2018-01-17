f34fe4933ba8cadf39e85b573bbc23b2b0902a13
Andrew <andrew.ramell@gmail.com>
2018-01-16 19:00:57 -0500

    Done.

---
+++ app/admin/songs.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Song do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :artist_id
+actions :all, except: [:destroy, :edit]
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  # resources :artists, :songs
+  get 'artists/index', to: 'artists#index'
+  get 'artists/:id', to: 'artists#show'
+  get 'songs/index', to: 'songs#index'
+  get 'songs/:id', to: 'songs#show'
 
   root to: 'artists#index'
 end
