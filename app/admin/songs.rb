0cb3f9f779ef8c6293d240fb3f2df3bb64de8139
mandlerm <amaya.melissa@gmail.com>
2018-01-23 16:34:55 -0600

    Done.

---
+++ app/admin/songs.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Song do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+ActiveAdmin.register Song do
+    permit_params :name, :genre
+     actions :all
+
+   form do |f|
+       inputs 'Song' do
+         f.input :name
+         f.input :genre
+       end
+       f.semantic_errors
+       f.actions
+     end
+end
+end
