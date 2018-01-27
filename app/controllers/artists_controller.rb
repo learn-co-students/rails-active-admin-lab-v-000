c36cec6d820401bd23a2c097b92956ec3c5da2d4
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-01-27 10:33:08 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,46 +1,53 @@
 class ArtistsController < ApplicationController
+  
   def index
   end
 
   def show
+    
+    @user = Artist.friendly.find(params[:id])
   end
 
-  def new
-    @artist = Artist.new
-  end
+  # def new
+  #   @artist = Artist.new
+    
+  #   redirect_to 'artist_path(#@artist.id)'
+  # end
 
-  def create
-    @artist = Artist.new(artist_params)
+  # def create
+  #   @artist = Artist.new(artist_params)
 
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
-  end
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def edit
-    @artist = Artist.find(params[:id])
-  end
+  # def edit
+  #   @artist = Artist.find(params[:id])
+  # end
 
-  def update
-    @artist = Artist.find(params[:id])
+  # def update
+  #   if !admin_signed_in?
+  #     @artist = Artist.find(params[:id])
+  
+  #     @artist.update(artist_params)
+  
+  #     if @artist.save
+  #       redirect_to @artist
+  #     else
+  #       render :edit
+  #     end
+  #   end
+  # end
 
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
+  # def destroy
+  #   @artist = Artist.find(params[:id])
+  #   @artist.destroy
+  #   flash[:notice] = "Artist deleted."
+  #   redirect_to artists_path
+  # end
 
   private
 
