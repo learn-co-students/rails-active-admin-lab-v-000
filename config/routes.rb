f0534fb0b5b852dc80620e5f4ad7a1eabec05167
CodeDad <github email address>
2018-02-02 11:44:19 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+  get '/artists', to: redirect('/')
+  resources :artists, except: [:edit, :new, :create, :update ] 
+  get '/songs', to: redirect('/')
+  resources :songs, except: [:edit, :new, :create, :update ] 
+  	
 
   root to: 'artists#index'
 end
+
