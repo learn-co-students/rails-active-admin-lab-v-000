a5086fd22808fb89e5a0cf4d018ec791c72dfc52
An Nuyen <annuyen@gmail.com>
2018-03-18 17:14:10 +0000

    Done.

---
+++ app/admin/artists.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Artist do
+  permit_params :name
+  actions :all, except: [:destroy]
+
+  form do |f|
+    inputs 'Artist' do
+      f.input :name
+    end
+    f.semantic_errors
+    f.actions
+  end
+end 
