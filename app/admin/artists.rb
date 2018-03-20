23f9aaba7d22cc9f30acf6c3625fba18ff37d2fa
americanmouths <shiverr@gmail.com>
2018-03-19 19:08:12 -0700

    Done.

---
+++ app/admin/artists.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register Artist do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  permit_params :names
+  actions :all, except: [:destroy]
+
+end
