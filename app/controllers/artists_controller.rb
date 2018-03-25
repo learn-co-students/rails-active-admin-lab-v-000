c96f57b8cbe36e5b38e2a95d19dcbcbfdeaf3630
chrisjamesr <chrisjames.r@gmail.com>
2018-03-25 13:34:43 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  before_action :is_admin?, only: [:new, :create, :edit, :update, :destroy]  
+  
   def index
     @artists = Artist.all
   end
@@ -7,43 +7,6 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
-
-  # def new
-  #   @artist = Artist.new
-  # end
-
-  # def create
-  #   @artist = Artist.new(artist_params)
-
-  #   if @artist.save
-  #     redirect_to @artist
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def edit
-  #   @artist = Artist.find(params[:id])
-  # end
-
-  # def update
-  #   @artist = Artist.find(params[:id])
-
-  #   @artist.update(artist_params)
-
-  #   if @artist.save
-  #     redirect_to @artist
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def destroy
-  #   @artist = Artist.find(params[:id])
-  #   @artist.destroy
-  #   flash[:notice] = "Artist deleted."
-  #   redirect_to artists_path
-  # end
 
   private
 
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-before_action :is_admin?, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
