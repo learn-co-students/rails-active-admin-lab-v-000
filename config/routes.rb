b8fb80aeb701a53592f57ecc088bd9f21a90ea7e
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-04-07 12:52:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
-  resources :artists, :songs
+
+  # TN: NEEDED TO CHANGE THIS...
+  # resources :artists, :songs
+
+  # TO THIS... SO THAT USERS COULD NOT EDIT/DELETE/ETC.
+  resources :artists, only: [:index, :show]
+  resources :songs, only: [:index, :show]
 
   root to: 'artists#index'
 end
